5234da33c0e3e2c2bd44bdcdc7ec711459afd819
Gowthamits <141577354+Gowthamits@users.noreply.github.com>
2023-08-28 22:35:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -28,10 +28,10 @@ class script(object):
 
     ABOUT_TXT = """<b>🤖 ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/qtmvefilrer_bot>🎋𝐏𝐮𝐥𝐥𝐢𝐧𝐠𝐨𝐨🎋</a>
 😈 𝐀𝐮𝐭𝐡𝐨𝐫 : <a href=https://t.me/Owner_of_qtmve>🍁𝐂𝐨𝐝𝐞𝐫🎎</a>
-👨‍🏫 𝐃𝐞𝐯𝐨𝐥𝐨𝐩𝐞𝐫 : <a href=https://t.me/SMD_Owner>🛸𝐕𝐚𝐫𝐢𝐲𝐚🚁</a>
+👨‍🏫 𝐃𝐞𝐯𝐨𝐥𝐨𝐩𝐞𝐫 : <a href=https://t.me/SMD_Owner>🌿𝐉𝐨𝐛𝐛𝐢 𝐂𝐫⚜️</a>
 ✍️ 𝐆𝐫𝐚𝐝𝐮𝐚𝐭𝐞 : <a href=https://t.me/Siva_Soft>🛬𝐌𝐚𝐚𝐦𝐚🚦</a>
-♠ 𝐖𝐢𝐟𝐞 𝐅𝐫𝐞𝐞: <a href=https://t.me/Svaraz>🎭𝐏𝐚𝐩𝐞𝐫 𝐢𝐝🔥</a>
-♣ 𝐇𝐮𝐬𝐛𝐚𝐧𝐝 𝐟𝐫𝐞𝐞: <a href=https://t.me/Thavarajtj>🦞𝐋𝐂𝐔🌿</a>
+♠ 𝐖𝐢𝐟𝐞 𝐅𝐫𝐞𝐞: <a href=https://t.me/Svaraz>🎭𝐌𝐚𝐜𝐡𝐢𝐢🔥</a>
+♣ 𝐇𝐮𝐬𝐛𝐚𝐧𝐝 𝐟𝐫𝐞𝐞: <a href=https://t.me/Thavarajtj>🦞𝐓𝐉 𝐁𝐡𝐚𝐢 🌿</a>
 ⚜️ 𝐐𝐭𝐯𝐬𝐨𝐟𝐟𝐢𝐜𝐢𝐚𝐥 : <a href=https://t.me/QTVS_BOT_X_CLOUD>🥀𝐌𝐚𝐫𝐯𝐞𝐥 𝐔𝐧𝐢𝐯𝐞𝐫𝐬𝐞🎋</a></b>"""
 
     SOURCES_TXT = """Tʜɪs Is Aɴ Oᴩᴇɴ-Sᴏᴜʀᴄᴇ Pʀᴏᴊᴇᴄᴛ Bʏ @SAM_DUB_LEZHA
