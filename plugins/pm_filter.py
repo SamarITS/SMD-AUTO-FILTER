3795c8c68d22fa517c147a58d1ff0f3377c08f96
Gowthamits <141577354+Gowthamits@users.noreply.github.com>
2023-08-29 14:10:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -781,14 +781,14 @@ async def auto_filter(client, msg, spoll=True):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
-             InlineKeyboardButton(text="Nᴇxᴛ ›››", callback_data=f"next_{req}_{key}_{offset}")]
+             InlineKeyboardButton(text="𝐍𝐞𝐱𝐭♣", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
     btn.insert(0, [
-        InlineKeyboardButton("🅟🅜 பாருங்க Ғʀɴᴅs 🥴", url=f"https://t.me/{temp.U_NAME}")
+        InlineKeyboardButton("🅟🅜 பாருங்க Ғʀɴᴅs 🥀", url=f"https://t.me/{temp.U_NAME}")
     ])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
