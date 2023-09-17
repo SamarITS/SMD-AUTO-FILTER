feb8359a6c44d06b4a8db468224e27f0eab14a03
SamarITS <115398030+SamarITS@users.noreply.github.com>
2023-09-17 17:52:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,8 +31,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://good:good@cluster0.m4vgkgm.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "good")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://samar10:samar10@cluster0.tau71xe.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "samar10")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
